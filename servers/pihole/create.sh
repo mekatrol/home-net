5ab5a0a87be9fcd3e951eed491ed4e2a882f5949
mekatrol <git@mekatrol.com>
2025-10-27 19:32:50 +1100

    Added pihole

---
+++ servers/pihole/create.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+set -euo pipefail
+
+# e.g. to run this script:
+# HOSTNAME="pihole.example.com" WEBPASSWORD="changeme" TIMEZONE="Australia/Sydney" ./create.sh
+
+if [ -z "$HOSTNAME" ]; then
+    echo "Error: HOSTNAME must be defined!"
+    exit 1
+fi
+
+if [ -z "$WEBPASSWORD" ]; then
+    echo "Error: WEBPASSWORD must be defined!"
+    exit 1
+fi
+
+if [ -z "$TIMEZONE" ]; then
+    echo "Error: TIMEZONE must be defined!"
+    exit 1
+fi
+
+# The name of the image that will be created with 'docker build'
+IMAGE_NAME="pihole"
+
+# The name of the container that will be created with docker run
+CONTAINER_NAME="pihole"
+
+# The name of the network the nginx server will use
+NETWORK_NAME="docker-network"
+
+# The driver method used when creating the network if it does not already exist
+NETWORK_DRIVER="ipvlan"
+
+# The network interface card used for the network
+NETWORK_PARENT="enp3s0"
+
+# The network subnet
+NETWORK_SUBNET="10.2.2.0/24"
+
+# The network gateway
+NETWORK_GATEWAY="10.2.2.1"
+
+# Static IP address for the server host
+CONTAINER_IP_ADDR="10.2.2.202"
+
+# PIHOLE volumes
+PIHOLE_VOLUME="/data/pihole-data:/etc/pihole"
+DNSMASQ_VOLUME="/data/pihole-dnsmasq.d:/etc/dnsmasq.d"
+
+# Make sure empty volumes exist
+rm -rf /data/pihole-data /data/pihole-dnsmasq.d
+mkdir -p /data/pihole-data /data/pihole-dnsmasq.d
+
+# Check if the network exists
+if ! docker network ls --format '{{.Name}}' | grep -q "^$NETWORK_NAME$"; then
+    echo "Network '$NETWORK_NAME' does not exist. Creating it..."
+    docker network create --driver="$NETWORK_DRIVER" --subnet="$NETWORK_SUBNET" --gateway="$NETWORK_GATEWAY" -o parent="$NETWORK_PARENT" "$NETWORK_NAME"
+else
+    echo "Network '$NETWORK_NAME' already exists."
+fi
+
+if ! docker image ls --format '{{.Tag}}' | grep -q "^$IMAGE_NAME$"; then
+    echo "Image '$IMAGE_NAME' does not exist. Creating it..."
+    docker build -t "$IMAGE_NAME" \
+        --build-arg HOSTNAME="$HOSTNAME" \
+        --build-arg TIMEZONE="$TIMEZONE" \
+        .
+else
+    echo "Image '$IMAGE_NAME' already exists."
+fi
+
+docker run \
+    -itd \
+    --restart=always \
+    --ip="$CONTAINER_IP_ADDR" \
+    -e WEBPASSWORD="$WEBPASSWORD" \
+    -e DNSMASQ_LISTENING=all \
+    -e PIHOLE_DNS_1=9.9.9.9 \
+    -e FTLCONF_LOCAL_IPV4="$CONTAINER_IP_ADDR" \
+    -e TZ="$TIMEZONE" \
+    --name="$CONTAINER_NAME" \
+    --hostname="$HOSTNAME" \
+    --network="$NETWORK_NAME" \
+    --volume="$PIHOLE_VOLUME" \
+    --volume="$DNSMASQ_VOLUME" \
+    "$IMAGE_NAME"
+
+printf 'Logs:      docker logs %q --tail=200\n' "$CONTAINER_NAME"
+printf 'Password:  docker exec -it %q %q setpassword %q\n' "$CONTAINER_NAME" "$CONTAINER_NAME" "$WEBPASSWORD"
