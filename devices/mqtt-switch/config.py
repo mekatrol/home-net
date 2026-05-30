6c14c9e9aad7fdfd08c99babc3920a6b19d2296d
Mekatrol <mekatrol@mekatrol.com>
2026-05-31 09:00:36 +1000

    Add pydevice folder

---
+++ devices/mqtt-switch/config.py
@@ -0,0 +1,12 @@
+config = {
+    "output_pin": 5,
+    "status_led_pin": "LED",
+    "wlan_ssid": "XXX",
+    "wlan_pwd": "XXX",
+    "time_server": "XXX",
+    "mqtt_host": "XXX",
+    "mqtt_username": "XXX",
+    "mqtt_password": "XXX",
+    "mqtt_status_topic": "XXX",
+    "mqtt_set_topic": "XXX"
+}
